a934298ed7664f884e03ec589aed54472132e4eb
rainie <rainietamakloe@gmail.com>
2024-11-07 11:39:18 +0000

    adding heading

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ function App() {
 
   return (
     <div className='header field'>
+      <h1>rainie's calc</h1>
       VAT CALCULATOR
       <div className='colour-border'>
         <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
